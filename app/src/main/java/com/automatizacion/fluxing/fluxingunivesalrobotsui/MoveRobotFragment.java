389284d352a7b78333fe31219bad76faf252273e
Diego Yael Vargas Becerra <yeyox1945@gmail.com>
2018-01-29 13:07:39 -0600

    proyecto con soporte de movimiento de robot con edittext + mejoras SQL e IP variable agregada

--- app/src/main/java/com/automatizacion/fluxing/fluxingunivesalrobotsui/MoveRobotFragment.java
+++ app/src/main/java/com/automatizacion/fluxing/fluxingunivesalrobotsui/MoveRobotFragment.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;

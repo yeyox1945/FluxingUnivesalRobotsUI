bd3864353d7c8419e1128af8be640d9ee3db2eae
Diego Yael Vargas Becerra <yeyox1945@gmail.com>
2018-02-12 09:40:24 -0600

    metodos de modificar y eliminar robots en la base de datos aun faltan correcciones de codigo

--- app/src/main/java/com/automatizacion/fluxing/fluxingunivesalrobotsui/AddRobotFragment.java
+++ app/src/main/java/com/automatizacion/fluxing/fluxingunivesalrobotsui/AddRobotFragment.java
@@ -8,12 +8,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public class AddRobotFragment extends Fragment {
@@ -26,8 +30,14 @@ public class AddRobotFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-
     private OnFragmentInteractionListener mListener;
+
+    public static ArrayList<String> RobotsList = new ArrayList<String>();
+    public static Spinner spnRobotsDB;
+    EditText Edit_Registro_Nombre;
+    EditText Edit_Registro_Modelo;
+    EditText Edit_Registro_IP;
+    EditText Edit_Registro_Dir;
 
     public AddRobotFragment() {
         // Required empty public constructor
@@ -56,42 +66,138 @@ public class AddRobotFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
         View view = inflater.inflate(R.layout.fragment_add_robot, container, false);
 
-        final EditText Edit_Registro_Nombre = view.findViewById(R.id.Edit_Registro_Nombre);
-        final EditText Edit_Registro_Modelo = view.findViewById(R.id.Edit_Registro_Modelo);
-        final EditText Edit_Registro_IP = view.findViewById(R.id.Edit_Registro_IP);
-        final EditText Edit_Registro_Dir = view.findViewById(R.id.Edit_Direccion_Programs);
+        Edit_Registro_Nombre = view.findViewById(R.id.Edit_Registro_Nombre);
+        Edit_Registro_Modelo = view.findViewById(R.id.Edit_Registro_Modelo);
+        Edit_Registro_IP = view.findViewById(R.id.Edit_Registro_IP);
+        Edit_Registro_Dir = view.findViewById(R.id.Edit_Direccion_Programs);
 
+        spnRobotsDB = view.findViewById(R.id.spnRobotsDB);
+        Fill_Spinner_Robots();
 
         Button BtnRegister = view.findViewById(R.id.buttonRegistrar);
-
         BtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 ConnectSQL SQL = new ConnectSQL();
-                Boolean Validate = SQL.RegisterRobot(Edit_Registro_Nombre.getText().toString(), Edit_Registro_Modelo.getText().toString(), Edit_Registro_IP.getText().toString(),Edit_Registro_Dir.getText().toString());
+                Boolean Validate = SQL.RegisterRobot(Edit_Registro_Nombre.getText().toString(),
+                        Edit_Registro_Modelo.getText().toString(),
+                        Edit_Registro_IP.getText().toString(),
+                        Edit_Registro_Dir.getText().toString());
 
-                Edit_Registro_Nombre.setText("");
-                Edit_Registro_Modelo.setText("");
-                Edit_Registro_IP.setText("");
-                Edit_Registro_Dir.setText("");
 
                 if(Validate){
                     Toast.makeText(getContext(), "Registro Exitoso",
                             Toast.LENGTH_LONG).show();
+
+                    Edit_Registro_Nombre.setText("");
+                    Edit_Registro_Modelo.setText("");
+                    Edit_Registro_IP.setText("");
+                    Edit_Registro_Dir.setText("");
+
+                    Fill_Spinner_Robots();
                 }else{
-                    Toast.makeText(getContext(), "Registro Erroneo",
-                            Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), "Registro Erroneo comprueba los campos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        return view;
+        Button BtnModify = view.findViewById(R.id.buttonModificar);
+        BtnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectSQL SQL = new ConnectSQL();
+                Boolean Validate = SQL.ModifyRobot(Edit_Registro_Nombre.getText().toString(),
+                        Edit_Registro_Modelo.getText().toString(),
+                        Edit_Registro_IP.getText().toString(),
+                        Edit_Registro_Dir.getText().toString());
 
+                if(Validate){
+                    Toast.makeText(getContext(), "Modificacion Exitosa",
+                            Toast.LENGTH_LONG).show();
+
+                    Edit_Registro_Nombre.setText("");
+                    Edit_Registro_Modelo.setText("");
+                    Edit_Registro_IP.setText("");
+                    Edit_Registro_Dir.setText("");
+
+                    Fill_Spinner_Robots();
+                }else{
+                    Toast.makeText(getContext(), "Modificacion Erronea comprueba los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button BtnDelete = view.findViewById(R.id.buttonEliminar);
+        BtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectSQL SQL = new ConnectSQL();
+                Boolean Validate = SQL.DeleteRobot(Edit_Registro_Nombre.getText().toString(),
+                        Edit_Registro_Modelo.getText().toString(),
+                        Edit_Registro_IP.getText().toString(),
+                        Edit_Registro_Dir.getText().toString());
+
+                if (Validate) {
+                    Toast.makeText(getContext(), "Eliminacion Exitosa", Toast.LENGTH_SHORT).show();
+
+                    Edit_Registro_Nombre.setText("");
+                    Edit_Registro_Modelo.setText("");
+                    Edit_Registro_IP.setText("");
+                    Edit_Registro_Dir.setText("");
+
+                    Fill_Spinner_Robots();
+                } else {
+                    Toast.makeText(getContext(), "Eliminacion Erronea comprueba los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        spnRobotsDB.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+
+                ///Agregar codigo de seleccion de ip aqui
+                String Robot = spnRobotsDB.getSelectedItem().toString();
+                String[] parts = Robot.split(" - ");
+
+                if (!Robot.equals("Modificar/Eliminar")) {
+
+                    Edit_Registro_Nombre.setText(parts[0]);
+                    Edit_Registro_Modelo.setText(parts[1]);
+                    Edit_Registro_IP.setText(parts[2]);
+                    Edit_Registro_Dir.setText(parts[3]);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        return view;
+    }
+
+    public void Fill_Spinner_Robots() {
+
+        RobotsList.clear();
+
+        //SpinnerRobot
+        spnRobotsDB.setAdapter(null);
+        RobotsList.add("Modificar/Eliminar");
+
+        //LLenar Con datos de SQL
+        ConnectSQL SQL = new ConnectSQL();
+        if (!SQL.Validate_Connection()) {
+            Toast.makeText(getContext(), "No se encontro una conexión a internet.", Toast.LENGTH_SHORT).show();
+        } else {
+            SQL.Fill_Combo_IP_RobotsSQL();
+
+            assert spnRobotsDB != null;
+            spnRobotsDB.setAdapter(new ArrayAdapter<>(getContext(), R.layout.spinner_style_items, RobotsList));
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -122,6 +228,4 @@ public class AddRobotFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
-
 }
--- app/src/main/java/com/automatizacion/fluxing/fluxingunivesalrobotsui/ConnectSQL.java
+++ app/src/main/java/com/automatizacion/fluxing/fluxingunivesalrobotsui/ConnectSQL.java
@@ -24,7 +24,6 @@ public class ConnectSQL {
     private String IP, DB, User, Password;
     public static String Robot = "";
 
-
     @SuppressLint("NewApi")
     public Connection ConnectSQL() {
 
@@ -62,9 +61,12 @@ public class ConnectSQL {
         return Connected;
     }
 
-    public Boolean RegisterRobot(String Name, String Model, String IP,String Directorio) {
+    public Boolean RegisterRobot(String Name, String Model, String IP, String Directorio) {
 
         boolean Validate = false;
+
+        if (Name.equals("") || Model.equals("") || IP.equals("") || Directorio.equals(""))
+            return Validate;
 
         Validate_Connection();
 
@@ -95,6 +97,70 @@ public class ConnectSQL {
         return Validate;
     }
 
+    public Boolean DeleteRobot(String Name, String Model, String IP, String Directorio) {
+
+        boolean Validate = false;
+
+        if (Name.equals("") || Model.equals("") || IP.equals("") || Directorio.equals(""))
+            return Validate;
+
+        Validate_Connection();
+
+        if (Validate_Connection() == false) {
+            System.out.println("No se pudo conectar a la BD");
+        } else {
+
+            try {
+                PreparedStatement pst = ConnectSQL().prepareStatement("DELETE FROM RegistroRobot WHERE Nombre=? AND Modelo=? AND IP=? AND Directorio=?");
+                pst.setString(1, Name);
+                pst.setString(2, Model);
+                pst.setString(3, IP);
+                pst.setString(4, Directorio);
+
+                pst.executeUpdate();
+
+                Validate = true;
+
+            } catch (SQLException e) {
+                System.out.println("SQL  error : " + e.getMessage());
+            } catch (NullPointerException e) {
+                System.out.println("null error : " + e.getMessage());
+            }
+        }
+        return Validate;
+    }
+
+    public Boolean ModifyRobot(String Name, String Model, String IP, String Directorio) {
+
+        boolean Validate = false;
+
+        if (Name.equals("") || Model.equals("") || IP.equals("") || Directorio.equals(""))
+            return Validate;
+
+        Validate_Connection();
+
+        if (Validate_Connection() == false) {
+            System.out.println("No se pudo conectar a la BD");
+        } else {
+
+            try {
+                PreparedStatement pst = ConnectSQL().prepareStatement("UPDATE RegistroRobot SET Nombre=");
+                pst.setString(1, Name);
+                pst.executeUpdate();
+
+                Validate = true;
+
+            } catch (SQLException e) {
+                //   Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                System.out.println("SQL  error : " + e.getMessage());
+            } catch (NullPointerException e) {
+                System.out.println("null error : " + e.getMessage());
+                // Toast.makeText(this, "Llena correctamente todos los campos", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return Validate;
+    }
+
     public static String dir = "/programs";
 
     public String Fill_Combo_IP_RobotsSQL() {
@@ -104,7 +170,6 @@ public class ConnectSQL {
         String Robot_for_Combobox = "";
 
         ConnectRobotFragment ConnectRobot = new ConnectRobotFragment();
-
 
         try {
             String Query = "SELECT * FROM [FluxingUniversalRobot].[dbo].[RegistroRobot] ORDER BY id;";
@@ -123,13 +188,13 @@ public class ConnectSQL {
                 Robot_for_Combobox = id + " - " + Nombre + " - " + Modelo + " - " + ip;
                 Robot = id + " - " + Nombre + " - " + Modelo + " - " + ip + " - " + dir;
 
-                System.out.println(Robot_for_Combobox);
                 ConnectRobot.RobotsList.add(Robot_for_Combobox);
+                AddRobotFragment.RobotsList.add(Robot_for_Combobox);
             }
         } catch (SQLException e) {
             System.out.println("Error al llenar Spinner :" + e.getMessage());
-        }catch (NullPointerException e){
-  }
+        } catch (NullPointerException e) {
+        }
         return Robot_for_Combobox;
     }
 
